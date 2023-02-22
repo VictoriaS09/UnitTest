3ec5e43e3fe593c5ea5e4f3d812a71b86d1f40d1
VictoriaS09 <125521730+VictoriaS09@users.noreply.github.com>
2023-02-21 20:00:33 -0500

    Add files via upload

---
+++ GitPracticeTwo.java
@@ -0,0 +1,16 @@
+package Test;
+
+public class GitPracticeTwo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println(testTwo(10, 5));
+	}
+
+	public static int testTwo(int a, int b) {
+		return a+b;
+	}
+	}
+
+
